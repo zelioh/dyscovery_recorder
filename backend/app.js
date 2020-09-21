6cf1633e91cba40eb7e1a68192603bacb93c4061
tanguy tournier <tanguy.tournier@epitech.eu>
2020-09-21 12:11:07 +0200

    Added public element like css and asset files

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set('views', path.join(__dirname, '../frontend/'));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/../public/'));
 
 app.get('/dyscovery_recorder', (req, res) => {
     res.render('index')
