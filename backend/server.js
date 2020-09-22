6411a03161e786ea14decff723c5da5eb383151d
zelioh <38160465+zelioh@users.noreply.github.com>
2020-09-22 21:19:12 +0200

    Added SSL

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 
 console.log('Server start !')
 
 app.set('port', process.env.PORT || 8042);
-const server = http.createServer(app);
 
-server.listen(process.env.PORT || 8042);
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(8042);
